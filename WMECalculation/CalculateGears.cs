e81e318a55a6d1172bb363c3a865e3f6e5148257
Taliston Ancelmo da Silva <bakura@live.co.uk>
2021-06-24 19:36:30 -0300

    some corrections

--- WMECalculation/CalculateGears.cs
+++ WMECalculation/CalculateGears.cs
@@ -119,7 +119,7 @@ namespace WMECalculation
             }
         }
 
-        public static void ApplyCalculatedGears(TextBox Qmax, TextBox Q07, TextBox Q04, TextBox Q025, TextBox Q015, TextBox Q01, TextBox Q005, TextBox Qmin, double resultFromGears, Image imgGear1, Image imgGear2, Label arrow, Label lbTittle, Button btnClear)
+        public static void ApplyCalculatedGears(TextBox Qmax, TextBox Q07, TextBox Q04, TextBox Q025, TextBox Q015, TextBox Q01, TextBox Q005, TextBox Qmin, double resultFromGears, Image imgGear1, Image imgGear2, Label lbTittle, Label arrow, Button btnClear)
         {
             if (Qmax.Text == "")
             {
